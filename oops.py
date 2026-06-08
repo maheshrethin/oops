df26f9d38e21bc8e4e10011471c86e009b6fbfda
maheshrethin <aswathirethin@gmail.com>
2026-06-08 17:26:24 +0530

    oops

--- oops.py
+++ oops.py
@@ -87,7 +87,7 @@
 # c=Car()
 # c.volvo()
 
-# polymorphism
+# polymorphism overriding
 # class Animal:
 #     def speak(self):
 #         print("Animal sound is louder")
@@ -101,3 +101,30 @@
 # D=Dog()
 # C.speak()
 # D.speak()
+
+
+# polymorphisim overloading
+# class sam:
+#     def Add(self,a):
+#         print(a)
+#     def Add(self,a,b):
+#         print(a+b)
+#     def Add(self,a,b,c):
+#         print(a+b+c)
+# s=sam()
+# # s.Add(10)
+# # s.Add(10,20)
+# s.Add(10,20,30)
+
+# class Load:
+#     def loading(self,a=None,b=None,c=None):
+#         if a!=None and b!=None and c!=None:
+#             return a+b+c
+#         elif a!=None and b!=None:
+#             return a+b
+#         else:
+#             return a
+# l=Load()
+# print("Add",l.loading(125))
+# print("Add",l.loading(125,125))
+# print("Add",l.loading(125,125,125))
