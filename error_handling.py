8155a0cc6d060e1e272c12cecdb63d917dd0745c
maheshrethin <aswathirethin@gmail.com>
2026-06-09 19:06:37 +0530

    error handling

---
+++ error_handling.py
@@ -0,0 +1,33 @@
+# error handling
+# types of error
+# type error
+# error occurs due to mentioning wrong data type 
+# name error
+# error occurs due to mentioning wrong name for ex declaring a and giving the value of b
+# syntax error
+# giving wrong syntax and declaring 
+# indentation error
+# error occurs due to space issue
+# index error
+# error occurs due to positional issue
+# key error
+# value error
+# error occus due to not giving the key value and value's value properly
+# zero division error
+
+# error sample
+# a=100
+# b=0
+# print(a/b)
+
+
+# name error sample
+
+# try:
+#     a=100
+#     b=0
+#     print(a/b)
+# except Exception as e:
+#     print(e)
+#     print('aswathi')
+
